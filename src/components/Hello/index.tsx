89ac847649e5cd291a62063f956b892a43085880
stefano da casa <ninotheking@gmail.com>
2022-06-18 11:30:35 +0200

    prime commit

--- src/components/Hello/index.tsx
+++ src/components/Hello/index.tsx
@@ -2,27 +2,9 @@ import logo from '~/logo.svg';
 
 const Hello = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        paddingTop: '50px',
-      }}
-    >
+    <div className='flex flex-column align-items-center h-screen justify-content-center'>
       <img src={logo} alt='Logo' style={{ height: '150px' }} />
-      <ul
-        style={{
-          padding: 0,
-          listStyle: 'none',
-          fontSize: '1.6rem',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '14px',
-        }}
-      >
+      <ul className='flex flex-column gap-2 text-center text-2xl list-none p-0'>
         <li>🚀 Vite</li>
         <li>🔥 React</li>
         <li>📖 TypeScript</li>
@@ -32,7 +14,7 @@ const Hello = () => {
       <p>Don&apos;t forgot to install Eslint and Prettier in your VSCode</p>
       <div>
         <a
-          style={{ color: '#F24C4C' }}
+          className='text-primary'
           target='_blank'
           href='https://github.com/igdev116/vite-react-ts-eslint-prettier'
           rel='noreferrer'
