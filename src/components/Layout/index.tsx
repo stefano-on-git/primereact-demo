fe76423505486e29511bf40ec76334c7dc424f76
stefano da casa <18739638+stefano-on-git@users.noreply.github.com>
2022-07-03 11:34:24 +0200

    layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,42 +1,57 @@
 import { useAuthContext } from '../Context/AuthContext';
-
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
 import { Menu } from 'primereact/menu';
 import { useRef } from 'react';
 import { Button } from 'primereact/button';
+import { MenuItem } from 'primereact/menuitem';
 
 const Layout = () => {
+  const optionsItems: MenuItem[] = [
+    {
+      label: 'New',
+      icon: 'pi pi-fw pi-plus',
+      command: () => {
+        window.location.hash = '/fileupload';
+      },
+    },
+    { label: 'Delete', icon: 'pi pi-fw pi-trash', url: 'http://primetek.com.tr' },
+  ];
+
+  const accountItems: MenuItem[] = [
+    {
+      label: 'Options',
+      icon: 'pi pi-fw pi-cog',
+      command: () => {
+        window.location.hash = '/';
+      },
+    },
+    {
+      label: 'Sign Out',
+      icon: 'pi pi-fw pi-power-off',
+      command: () => {
+        confirmDialog({
+          message: 'Sei sicuro di voler procedere?',
+          header: 'Conferma sign out',
+          icon: 'pi pi-exclamation-triangle',
+          accept: () => {
+            setIsAuth!(false);
+          },
+          reject: () => {
+            return false;
+          },
+        });
+      },
+    },
+  ];
+
   const items = [
     {
       label: 'Options',
-      items: [
-        {
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-          command: () => {
-            window.location.hash = '/fileupload';
-          },
-        },
-        { label: 'Delete', icon: 'pi pi-fw pi-trash', url: 'http://primetek.com.tr' },
-      ],
+      items: optionsItems,
     },
     {
       label: 'Account',
-      items: [
-        {
-          label: 'Options',
-          icon: 'pi pi-fw pi-cog',
-          command: () => {
-            window.location.hash = '/';
-          },
-        },
-        {
-          label: 'Sign Out',
-          icon: 'pi pi-fw pi-power-off',
-          command: () => {
-            setIsAuth!(false);
-          },
-        },
-      ],
+      items: accountItems,
     },
   ];
 
@@ -45,9 +60,9 @@ const Layout = () => {
 
   return (
     <div className='flex flex-column overflow-hidden h-screen'>
-      <div className='shadow-4 surface-card border-bottom-1 surface-border p-2'>
-        <div className='flex justify-content-between flex-row align-items-center'>
-          <div className='flex align-items-center'>
+      <div className='shadow-4 surface-card border-bottom-1 surface-border'>
+        <div className='flex justify-content-start flex-row align-items-center'>
+          <div className='flex align-items-center border-right-1 border-black-alpha-50 surface-0 shadow-2 py-3 px-5'>
             <img
               src='images/octopus.svg'
               alt='hyper'
@@ -61,15 +76,15 @@ const Layout = () => {
                 fontFamily: 'Cookie',
                 textShadow: '0 2px 1px #000',
               }}
-              className='text-2xl ml-2'
+              className='text-3xl ml-2'
             >
               Octopus Condominio
             </div>
           </div>
-          <div className='flex flex-grow-1 justify-content-end'>
+          <div className='flex align-items-center p-3 border-left-1 surface-border'>
             <Button
               className='flex'
-              label='Menu'
+              label=''
               icon='pi pi-bars'
               onClick={(event) => menu.current!.toggle(event)}
               aria-controls='popup_menu'
@@ -78,6 +93,7 @@ const Layout = () => {
           </div>
           <div className='flex align-items-center'>
             <Menu model={items} popup ref={menu} />
+            <ConfirmDialog />
           </div>
         </div>
       </div>
